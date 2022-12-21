dcae222e0806f2896790d9353dd64c9a3e63336b
Bruno Sajermann <sajermannbruno@gmail.com>
2022-12-21 08:54:56 -0300

    adjusts

--- src/Components/Filter/SuperFilter/index.tsx
+++ src/Components/Filter/SuperFilter/index.tsx
@@ -1,13 +1,24 @@
 import { useState } from 'react';
 import { Button } from '~/Components/Button';
+import { Input } from '~/Components/Input';
 import { Modal } from '~/Components/Modal';
 import { SelectNew } from '~/Components/SelectNew';
 import { useTranslation } from '~/Hooks/UseTranslation';
+import { generateGuid } from '@sajermann/utils/Random';
+
+type FilterActive = {
+	id: string;
+	column: string;
+	type: string;
+	value: string;
+};
 
 export function SuperFilter() {
 	const [isOpenModal, setIsOpenModal] = useState(false);
 	const [optionColumnSelected, setOptionColumnSelected] = useState('id');
 	const [optionTypeSelected, setOptionTypeSelected] = useState('equals');
+	const [valueSelected, setValueSelected] = useState('');
+	const [activeFilters, setActiveFilters] = useState<FilterActive[]>([]);
 	const { translate } = useTranslation();
 
 	const optionsColumns = [
@@ -20,8 +31,20 @@ export function SuperFilter() {
 		{ value: 'equals', label: translate('EQUAL') },
 		{ value: 'bigger', label: translate('BIGGER_THAN') },
 		{ value: 'smaller', label: translate('SMALLER_THAN') },
-		{ value: 'different', label: translate('DIFERENT') },
+		{ value: 'different', label: translate('DIFFERENT') },
 	];
+
+	function handleAddFilter() {
+		setActiveFilters(old => [
+			...old,
+			{
+				id: generateGuid(),
+				column: optionColumnSelected,
+				type: optionTypeSelected,
+				value: valueSelected,
+			},
+		]);
+	}
 
 	return (
 		<>
@@ -36,30 +59,49 @@ export function SuperFilter() {
 				onClose={() => setIsOpenModal(false)}
 				closeButton
 			>
-				<SelectNew
-					isClearable
-					options={optionsColumns}
-					value={
-						optionsColumns.find(item => item.value === optionColumnSelected)
-							?.value
-					}
-					onChange={e => setOptionColumnSelected(e.target.value)}
-					id="select_type"
-					placeholder={translate('FILTER_TYPE')}
-				/>
+				<div className="grid grid-cols-12 gap-4">
+					<div className="col-span-6">
+						<SelectNew
+							label={translate('COLUMN')}
+							isClearable
+							options={optionsColumns}
+							value={
+								optionsColumns.find(item => item.value === optionColumnSelected)
+									?.value
+							}
+							onChange={e => setOptionColumnSelected(e.target.value)}
+							id="select_type"
+							placeholder={translate('FILTER_TYPE')}
+						/>
+					</div>
+					<div className="col-span-6">
+						<SelectNew
+							label={translate('TYPE_FILTER')}
+							isClearable
+							options={optionsType}
+							value={
+								optionsType.find(item => item.value === optionTypeSelected)
+									?.value
+							}
+							onChange={e => setOptionTypeSelected(e.target.value)}
+							id="select_type"
+							placeholder={translate('FILTER_TYPE')}
+						/>
+					</div>
+					<Input
+						placeholder={translate('VALUE')}
+						value={valueSelected}
+						onChange={e => setValueSelected(e.target.value)}
+					/>
+				</div>
 
-				<SelectNew
-					isClearable
-					options={optionsType}
-					value={
-						optionsType.find(item => item.value === optionTypeSelected)?.value
-					}
-					onChange={e => setOptionTypeSelected(e.target.value)}
-					id="select_type"
-					placeholder={translate('FILTER_TYPE')}
-				/>
+				<Button onClick={handleAddFilter}>Adicionar Filtro</Button>
 
-				{JSON.stringify({ optionColumnSelected, optionTypeSelected }, null, 2)}
+				{JSON.stringify(
+					{ optionColumnSelected, optionTypeSelected, activeFilters },
+					null,
+					2
+				)}
 			</Modal>
 		</>
 	);
